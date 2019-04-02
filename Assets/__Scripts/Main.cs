20e4aaa82324b32ef003da49439f1446d2f2e0d6
svadkar <svadkar725@gmail.com>
2019-04-02 09:38:08 -0400

    page 510

---
+++ Assets/__Scripts/Main.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    static public Main S;
+
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;  //# Enemies/Second
+    public float enemySpawnPadding = 1.5f;     //Padding for position
+    public bool ______________________;
+
+    public float enemySpawnRate;   //Delay between ememy spawns
+
+    void Awake()
+    {
+        S = this;
+        //Set Utils.camBounds
+        Utils.SetCameraBounds(this.GetComponent<Camera>());
+        //0.5 enemies/second = enemySpawnRate of 2
+        enemySpawnRate = 1f/enemySpawnPerSecond;
+        //Invoke call SpawnEnemy() once after a 2 second delay
+        Invoke("SpawnEnemy", enemySpawnRate);
+    }
+
+    public void SpawnEnemy()
+    {
+        //Pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate(prefabEnemies[ndx]) as GameObject;
+        //Position the Enemy above the screen with a randomx positioon
+        Vector3 pos = Vector3.zero;
+        float xMin = Utils.camBounds.min.x + enemySpawnPadding;
+        float xMax = Utils.camBounds.max.x - enemySpawnPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = Utils.camBounds.max.y + enemySpawnPadding;
+        go.transform.position = pos;
+        //Call SpawnEnemy() again in a couple of seconds
+        Invoke("SpawnEnemy", enemySpawnRate);
+    }
+}
