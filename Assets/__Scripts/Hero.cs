9753ec79c02b6075ec1eed7512ea3c2f98df77cc
svadkar <svadkar725@gmail.com>
2019-04-02 10:59:53 -0400

    pg 519

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Hero : MonoBehaviour {
     static public Hero S;   //Singleton
 
+    public float gameRestartDelay = 2f;
+
     //These fields control the movement of the ship
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
 
     //Ship status information
-    public float shieldLevel = 1;
+    [SerializeField]
+    private float _shieldLevel = 1;
+
+
     public bool ________________;
     public Bounds bounds;
 
@@ -53,8 +58,63 @@ public class Hero : MonoBehaviour {
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
 	}
 
-    private void OnTriggerEnter(Collider other)
+    //This variable hold a reference to the last triggering GameObject
+    public GameObject lastTriggerGo = null;
+
+    void OnTriggerEnter(Collider other)
     {
-        print("Triggered: " + other.gameObject.name);
+        //Find the tag of other.gameObject or its parent GameObjects
+        GameObject go = Utils.FindTaggedParent(other.gameObject);
+
+        //If there is a parent with a tag
+        if (go != null)
+        {
+            //Make sure it's not the same triggering go as last time
+            if(go == lastTriggerGo)
+            {
+                return;
+            }
+
+            lastTriggerGo = go;
+
+            if (go.tag == "Enemy")
+            {
+                //If the shield was triggered by an enemy, decrease the level of the shield by 1
+                shieldLevel--;
+                //Destroy the enemy
+                Destroy(go);
+            }
+            else
+            {
+                print("Triggered: " + go.name);
+            }
+            
+        }
+
+        else
+        {
+            //Otherwise announce the original other.gameObject
+            print("Triggered: " + other.gameObject.name);
+        }
+    }
+
+    public float shieldLevel
+    {
+        get
+        {
+            return (_shieldLevel);
+        }
+
+        set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+            //if the shield is going to be set to less than zero
+            if (value < 0)
+            {
+                Destroy(this.gameObject);
+                //Tell Main.S to restart the game after a delay
+                Main.S.DelayedRestart(gameRestartDelay);
+            }
+        }
     }
 }
