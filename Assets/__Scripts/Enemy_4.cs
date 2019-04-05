91e981174a136b433aab55e8e8d9adcbe55b8651
svadkar <svadkar725@gmail.com>
2019-04-05 19:03:02 -0400

    finished enemy_4

--- Assets/__Scripts/Enemy_4.cs
+++ Assets/__Scripts/Enemy_4.cs
@@ -83,4 +83,144 @@ public class Enemy_4 : Enemy {
 
         pos = (1 - u) * points[0] + u * points[1];
     }
+
+    //This will override the OnCollisionENter that is part of Enemy.cs
+    //Because of the way that MonoBehaviour declares common Unity functions
+    //  like OnCollisionEnter(), the override keyword is not necessary.
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject other = coll.gameObject;
+        switch (other.tag)
+        {
+            case "ProjectileHero":
+                Projectile p = other.GetComponent<Projectile>();
+                //Enemies don't take damage unless they're on screen
+                //This stops the player from shooting them before they are visible
+                bounds.center = transform.position + boundsCenterOffset;
+                if(bounds.extents == Vector3.zero || Utils.ScreenBoundsCheck(bounds, BoundsTest.offScreen) != Vector3.zero)
+                {
+                    Destroy(other);
+                    break;
+                }
+
+                //Hurt this enemy
+                //Find the GameObject that was hit
+                //The Collision coll has contacts[], an array of ContactPoints
+                //Because there was a collision, we're guaranteed that there is at least a contacts[0],
+                //  and ContactPoints have a reference to thisCollider, which will be the collider for the part
+                //  of the Enemy_4 that was hit.
+                GameObject goHit = coll.contacts[0].thisCollider.gameObject;
+                Part prtHit = FindPart(goHit);
+                if (prtHit == null)
+                {
+                    //If prtHit wasn't found then it's usually becasue, very rarely, thisCollider on
+                    //  contacts[0] will be the ProjectileHero instead of the ship part. If so, just look for
+                    //  otherCollider insteasd
+                    goHit = coll.contacts[0].otherCollider.gameObject;
+                    prtHit = FindPart(goHit);
+                }
+
+                //Check whether this part is still protected
+                if (prtHit.protectedBy != null)
+                {
+                    foreach(string s in prtHit.protectedBy)
+                    {
+                        //If one of the protecting parts hasn't been destroyed...
+                        if (!Destroyed(s))
+                        {
+                            //...then don't damage this part yet
+                            Destroy(other);             //Destroy the ProjectileHero
+                            return;                     //Return before causing damage.
+                        }
+                    }
+                }
+
+                //It's not protected, so make it take damage
+                //Get the damage amount from the Projectile.type & Main.W_DEFS
+                prtHit.health -= Main.W_DEFS[p.type].damageOnHit;
+                //Show damage on the part
+                ShowLocalizedDamage(prtHit.mat);
+                if (prtHit.health <= 0)
+                {
+                    //Instead of Destroying this enemy, disable the damaged part
+                    prtHit.go.SetActive(false);
+                }
+
+                //check to see if the whole ship is destroyed
+                bool allDestroyed = true;   //Assume it is destroyed
+                foreach (Part prt in parts)
+                {
+                    if (!Destroyed(prt))            //If a part still exists...
+                    {
+                        allDestroyed = false;       //...change allDestroyed to false
+                        break;                      //and break out of the foreach loop
+                    }
+                }
+
+                if (allDestroyed)
+                {
+                    //If it IS completely detroyed
+                    //Tell the Main singleton that this ship has been destroyed
+                    Main.S.ShipDestroyed(this);
+                    //Destroy this Enemy
+                    Destroy(this.gameObject);
+                }
+                Destroy(other); //Destroy the ProjectileHero
+                break;
+        }
+    }
+
+    //These two functions find a Part in parts based on name or GameObject
+    Part FindPart (string n)
+    {
+        foreach (Part prt in parts)
+        {
+            if (prt.name == n)
+            {
+                return (prt);
+            }
+        }
+        return (null);
+    }
+    Part FindPart(GameObject go)
+    {
+        foreach (Part prt in parts)
+        {
+            if (prt.go == go)
+            {
+                return (prt);
+            }
+        }
+        return (null);
+    }
+
+    //These functions return true if the part has been destroyed
+    bool Destroyed (GameObject go)
+    {
+        return (Destroyed(FindPart(go)));
+    }
+
+    bool Destroyed(string n)
+    {
+        return (Destroyed(FindPart(n)));
+    }
+
+    bool Destroyed(Part prt)
+    {
+        if(prt == null)     //If no real ph was passed in
+        {
+            return (true);  //Return true (meaning yes, it was destroyed)
+        }
+        //Returns the result of the comparison: prt.health <= 0
+        //If prt.health is 0 or less, returns true (yes, it was destroyed)
+        return (prt.health <= 0);
+    }
+
+    //This changes the color of just one Part to red instead of the whole setup
+    void ShowLocalizedDamage(Material m)
+    {
+        m.color = Color.red;
+        remainingDamageFrames = showDamageForFrames;
+    }
 }
