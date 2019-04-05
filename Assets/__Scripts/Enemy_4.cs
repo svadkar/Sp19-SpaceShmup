3ade27e480d0f4ce68fae51afac827a1c357996f
svadkar <svadkar725@gmail.com>
2019-04-05 18:10:31 -0400

    started enemy_4.cs: pg 550

--- Assets/__Scripts/Enemy_4.cs
+++ Assets/__Scripts/Enemy_4.cs
@@ -2,6 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Part is another serializable data storage class jsut like WeaponDefinition
+[System.Serializable]
+public class Part
+{
+    //These three fields need to be defined in the Inspector pane
+    public string name;                 //The name of this part
+    public float health;                //The amount of health this part has
+    public string[] protectedBy;        //The other parts that protect this
+
+    //These two fields are set automatically in Start();
+    //Caching like this makes it faster and easier to find these later
+    public GameObject go;           //The GameObject of this part
+    public Material mat;            //The Material to show damage.
+}
+
 public class Enemy_4 : MonoBehaviour {
 
 	// Use this for initialization
